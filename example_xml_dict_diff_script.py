7dd7d9da743e1373ef2077f7bf76a71e3f77105b
ashshah <ashna.shah.1426@gmail.com>
2021-03-12 15:16:53 -0800

    changing to defaults

--- example_xml_dict_diff_script.py
+++ example_xml_dict_diff_script.py
@@ -3,13 +3,12 @@ from napalm import get_network_driver
 # import napalm driver and open the connection
 driver = get_network_driver("sros")
 optional_args = {"port": 830}
-device = driver("138.120.181.55", "admin", "admin", 60, optional_args)
-# device = driver("127.0.0.1", "vagrant", "vagrant", 60, optional_args)
+device = driver("127.0.0.1", "vagrant", "vagrant", 60, optional_args)
 
 device.open()
 
 # call load_replace
-device.load_replace_candidate(filename="config_1005_lines_new_180.xml")
+device.load_replace_candidate(filename="filename")
 
 
 # xml diff between candidate and running
