f44e6e28d5451ddc0c5437362dae5eb95f471d64
ashshah <ashna.shah.1426@gmail.com>
2021-03-12 15:09:09 -0800

    changes made in method's signature and unit tests written for get_config in XML

--- example_xml_dict_diff_script.py
+++ example_xml_dict_diff_script.py
@@ -3,24 +3,21 @@ from napalm import get_network_driver
 # import napalm driver and open the connection
 driver = get_network_driver("sros")
 optional_args = {"port": 830}
-device = driver("127.0.0.1", "vagrant", "vagrant", 60, optional_args)
+device = driver("138.120.181.55", "admin", "admin", 60, optional_args)
+# device = driver("127.0.0.1", "vagrant", "vagrant", 60, optional_args)
 
 device.open()
 
-# get the config in XML format
-config = device.get_config()
-
 # call load_replace
-device.load_replace_candidate(filename="filename")
+device.load_replace_candidate(filename="config_1005_lines_new_180.xml")
 
-# get the candidate config in XML format
-candidate_config = device.get_config(retrieve="candidate")
 
 # xml diff between candidate and running
-print(device.compare_config(running_config=config["running"], candidate_config=candidate_config["candidate"]))
+print(device.compare_config())
 
 # commit the config
 device.commit_config()
 
 # closing the device
 device.close()
+print("device closed")
