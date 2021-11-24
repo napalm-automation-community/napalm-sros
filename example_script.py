3e478bff39a7f5a8e5f692e4c0a21ecfcb2ee8d9
ashshah <ashna.shah.1426@gmail.com>
2021-11-24 15:43:54 -0800

    Modifications made according to standard practices

--- example_script.py
+++ example_script.py
@@ -1,20 +1,11 @@
-# from napalm import get_network_driver
-#
-# driver = get_network_driver("sros")
-# optional_args = {'port': 830}
-# device = driver("127.0.0.1", "vagrant", "vagrant", 60, optional_args)
-# device.open()
-# print(device.get_facts())
-# print(device.get_optics())
-# device.close()
-
-
 from napalm import get_network_driver
 
 driver = get_network_driver("sros")
 optional_args = {'port': 830}
-device = driver("10.1.0.101", "admin", "admin", 60, optional_args)
+device = driver("127.0.0.1", "admin", "admin", 60, optional_args)
 device.open()
-print(device.get_config())
-# print(device.get_optics())
+print(device.get_facts())
+print(device.get_optics())
 device.close()
+
+
